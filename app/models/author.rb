6d7ad0c623545df3406a14eb4bfed982e683eaff
hahm-d <dhahm01@gmail.com>
2020-07-21 13:46:10 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ActiveRecord::Base
+    has_many :posts
+
+    #validate email is unique
+    validates :email, uniqueness: true
+    #name cannot be blank!  
+    validates :name, presence: true
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+    belongs_to :author
+
+    #category is either 'Fiction' or 'Non-Fiction'
+    validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+    #content < 100 characters
+    validates :content, length: { minimum: 100}
+    #post must have a title
+    validates :title, presence: true
 end
